2b3aff930f675e2603aeb87f8f0763b51d226515
Jatin Singh <jatinsingh6654@gmail.com>
2026-02-11 09:42:20 +0530

    testing

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <time.h>
+// this is test comment 1
+// this is test comment 2
 
 char* date_formatter(char* date);
 char* time_unit_converter(int hrs);
